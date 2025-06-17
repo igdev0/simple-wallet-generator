b70c7b795a7916a021c5b3b5e522cb2e11f4eb5c
igdev <dorultanianos@gmail.com>
2025-06-17 15:14:18 +0300

    feat: wallet redux store

--- src/store/wallet.ts
+++ src/store/wallet.ts
@@ -2,23 +2,62 @@ import {createSlice} from '@reduxjs/toolkit';
 import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import type {PersistConfig} from 'redux-persist/es/types';
+import {HDNodeWallet} from 'ethers';
 
-export interface WalletStore {
+interface Account {
+  name: string;
+  path: string;
+  address: string;
 }
 
-const initialState: WalletStore = {};
+interface State {
+  encryptedMaster: string | null;
+  accounts: Account[];
+  error: string | null;
+  isLocked: boolean;
+}
+
+const initialState: State = {
+  encryptedMaster: null,
+  accounts: [],
+  isLocked: true,
+  error: null,
+};
 
 const walletSlice = createSlice({
   name: "wallet",
   initialState,
-  reducers: {}
-});
+  reducers: {
 
+    setEncryptedMaster(state, action: { payload: string }) {
+      // Randomly generate wallet
+      state.encryptedMaster = action.payload;
+    },
+
+    setLocked(state, action: { payload: boolean }) {
+      state.isLocked = action.payload;
+    },
+
+    setError(state, action: { payload: string }) {
+      state.error = action.payload;
+    },
+
+    generateAccount(state, action: { payload: HDNodeWallet }) {
+      state.accounts.push({
+        name: `Account ${state.accounts.length}`,
+        path: action.payload.path as string,
+        address: action.payload.address,
+      });
+    },
+  }
+});
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: [],
+  whitelist: ['accounts', 'encryptedMaster'],
 } as PersistConfig<any>;
+
+export const {generateAccount, setLocked, setError, setEncryptedMaster} = walletSlice.actions;
 
 export default persistReducer(persistConfig, walletSlice.reducer);
