7750d15e3de97b01d00d05d849a796920b8fa0aa
igdev <dorultanianos@gmail.com>
2025-06-17 20:45:27 +0300

    feat: add Jest configuration for TypeScript support with ts-jest

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+/** @jest-config-loader ts-node */
+/** @jest-config-loader-options {"transpileOnly": true} */
+
+import type {Config} from 'jest';
+import {createDefaultPreset} from 'ts-jest';
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+const config: Config = {
+  preset: "ts-jest",
+  verbose: true,
+  transform: {
+    ...tsJestTransformCfg
+  }
+};
+
+export default config;
